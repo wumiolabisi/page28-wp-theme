d56b8207871531e0393c0eee48f804cd90e206f6
unknown <wumi.olabisi@gmail.com>
2023-09-23 19:28:50 +0200

    Intégration du design dans la boucle WP pour la première bannière

--- front-page.php
+++ front-page.php
@@ -4,45 +4,46 @@
 
 get_header(); ?>
 
+
 <div class="p28-container">
-    <div class="p28-pinnedpost p28-bg-15071d">
-        <div class="p28-pinnedpost-left">
-            <h1 class="p28-txt-cbbdff">LE TITRE</h1>
-            <p class="p28-txt-cbbdff">Blablabla</p>
-            <a class="p28-btn p28-btn-primary">TEST BOUTON</a>
-        </div>
-        <div class="p28-flexrowend p28-angled-right p28-bg-15071d">
-            <div class="p28-bar p28-bg-fec32e"></div>
-            <div class="p28-bar p28-bg-cbbdff"></div>
-        </div>
-        <div class="p28-pinnedpost-right">
-            <img class="p28-imgfull" src="http://localhost/page28-local/wp-content/uploads/2023/09/portrait-de-la-jeune-fille-en-feu-ou-comment-regarder-autrement1-759x500-1.jpg" />
-        </div>
-    </div>
+    <?php
+    $args  = array(
+        'post_type'           => 'oeuvres',
+        'posts_per_page'      => 1
+    );
+    $query = new WP_Query($args);
 
+    if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+
+
+
+            <div class="p28-pinnedpost p28-bg-15071d">
+                <div class="p28-pinnedpost-left">
+                    <h1 class="p28-txt-cbbdff"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
+                    <div class="p28-txt-cbbdff"><?php the_excerpt(); ?></div>
+                    <a href="<?php the_permalink(); ?>" class="p28-btn p28-btn-primary">VOIR LA FICHE</a>
+
+                </div>
+                <div class="p28-flexrowend p28-angled-right p28-bg-15071d">
+                    <div class="p28-bar p28-bg-fec32e"></div>
+                    <div class="p28-bar p28-bg-cbbdff"></div>
+                </div>
+                <div class="p28-pinnedpost-right">
+                    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('post-thumbnail', ['class' => 'p28-imgfull']); ?></a>
+                </div>
+            </div>
+
+
+
+
+
+
+
+    <?php
+        endwhile;
+    endif;
+
+    wp_reset_postdata();
+    ?>
 </div>
-
-<?php
-$args  = array(
-    'post_type'           => 'oeuvres',
-    'posts_per_page'      => 1
-);
-$query = new WP_Query($args);
-
-// 3. On lance la boucle !
-if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
-
-        the_title();
-        the_excerpt();
-        the_post_thumbnail(); ?>
-        <p>
-            <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
-        </p>
-<?php
-    endwhile;
-endif;
-
-// 4. On réinitialise à la requête principale (important)
-wp_reset_postdata();
-?>
 <?php get_footer(); ?>
