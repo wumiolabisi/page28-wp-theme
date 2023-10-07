51c070ada2ae38ed55cd2d50558a6acb30013c9e
unknown <wumi.olabisi@gmail.com>
2023-10-07 15:29:52 +0200

    Scindage du functions.php pour plus de clarté

---
+++ inc/assets.php
@@ -0,0 +1,45 @@
+<?php
+function page28_register_assets()
+{
+
+    // Déclarer jQuery
+    wp_enqueue_script('jquery');
+
+    // Déclarer le JS
+    wp_enqueue_script(
+        'header',
+        get_template_directory_uri() . '/assets/header.js',
+        array('jquery'),
+        '1.0',
+        true
+    );
+
+
+
+    // Déclarer le fichier CSS à un autre emplacement
+    wp_enqueue_style(
+        'style',
+        get_template_directory_uri() . '/style.css',
+        array(),
+        '1.0'
+    );
+    wp_enqueue_style(
+        'general',
+        get_template_directory_uri() . '/assets/general.css',
+        array(),
+        '1.0'
+    );
+    wp_enqueue_style(
+        'header',
+        get_template_directory_uri() . '/assets/header.css',
+        array(),
+        '1.0'
+    );
+    wp_enqueue_style(
+        'front-page',
+        get_template_directory_uri() . '/assets/front-page.css',
+        array(),
+        '1.0'
+    );
+}
+add_action('wp_enqueue_scripts', 'page28_register_assets');
