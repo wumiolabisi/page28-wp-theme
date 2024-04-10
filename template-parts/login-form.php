ff4f47b12c6f3ff5d3ab11631f658ad7f95fc04a
unknown <wumi.olabisi@gmail.com>
2024-04-10 11:53:15 +0200

    Mise à jour à partir du site en ligne

---
+++ template-parts/login-form.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/logo-page-28.png" alt="Logo Page 28" class="p28-50">
+    </div>
+    <div>
+        <form></form>
+    </div>
+</div>
